dd3e45ec9390f508643589792ba77fa67215f904
siva <siva1978@gmail1.com>
2020-03-28 18:39:14 +0530

    added a methd to avoid duplicate code

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import Movie from "../movie/Movie";
 import "./styles.css";
 class Movies extends React.Component {
+  getMovie = (name, likes, banner) => {
+    return <Movie name={name} likes={likes} banner={banner} />;
+  };
   render() {
     return (
       <div className="movies-section">
-        <Movie
-          likes="123.0k"
-          name="Sarainodu"
-          banner="https://www.filmibeat.com/fanimg/214x100x267/fan_images/sarrainodu-photos-images-41556.jpg"
-          hero="Alluarjun"
-        />
-        <Movie
-          likes="123.0k"
-          name="Maharshi"
-          banner="https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg"
-          hero=""
-        />
-        <Movie
-          likes="1m"
-          name="Avengers"
-          banner="https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
-          hero=""
-        />
+        {this.getMovie(
+          "Sarainodu",
+          "123.0k",
+          "https://www.filmibeat.com/fanimg/214x100x267/fan_images/sarrainodu-photos-images-41556.jpg"
+        )}
+        {this.getMovie(
+          "Maharshi",
+          "123.0k",
+          "https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg"
+        )}
+        {this.getMovie(
+          "Avengers",
+          "1m",
+          "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
+        )}
       </div>
     );
   }
