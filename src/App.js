34a43009551f8332a121f79dd6b2f2b77fddf915
siva <siva1978@gmail1.com>
2020-03-29 12:42:03 +0530

    Added new LifeCycleDemo Component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import ListCoursesComponentApp from "./components/ListCoursesComponentApp";
 import Movie from "./movie/Movie";
 import Movies from "./Movies/Movies";
 import Calculator from "./Calculator/Calculator";
+import LifeCycleDemo from "./LifeCycleDemo/LifeCycleDemo";
 
 function App() {
   return (
     <div className="containter">
-      <Calculator />
-      {/*
-      <Movies />
+      {/*<Calculator />*/}
+      {/*{
+      <Movies />}
       */}
+      <LifeCycleDemo />
     </div>
   );
 }
