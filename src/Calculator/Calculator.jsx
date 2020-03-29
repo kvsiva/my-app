96c618409b50665a211279f60591d5bad9fddbe4
siva <siva1978@gmail1.com>
2020-03-29 12:21:14 +0530

    updated calculator component

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -2,18 +2,27 @@ import React from "react";
 class Calculator extends React.Component {
   state = {
     a: 10,
-    b: 5
+    b: 5,
+    showResult: false
   };
 
   setA = e => {
     this.setState({
-      a: parseInt(e.target.value)
+      a: parseInt(e.target.value),
+      showResult: false
     });
   };
 
   setB = e => {
     this.setState({
-      b: parseInt(e.target.value)
+      b: parseInt(e.target.value),
+      showResult: false
+    });
+  };
+
+  setShowResultToTrue = e => {
+    this.setState(prevState => {
+      return { showResult: !prevState.showResult };
     });
   };
   render() {
@@ -21,15 +30,22 @@ class Calculator extends React.Component {
       <div>
         <label> A: </label> <input onChange={this.setA} />
         <label> B: </label> <input onChange={this.setB} />
+        <button onClick={this.setShowResultToTrue}>Result</button>
         <br />
-        {"Add:" + (this.state.a + this.state.b)}
-        <br />
-        {"Sub:" + (this.state.a - this.state.b)}
-        <br />
-        {"Mul:" + this.state.a * this.state.b}
-        <br />
-        {"Div:" + this.state.a / this.state.b}
-        <br />
+        {this.state.showResult ? (
+          <div>
+            {"Add:" + (this.state.a + this.state.b)}
+            <br />
+            {"Sub:" + (this.state.a - this.state.b)}
+            <br />
+            {"Mul:" + this.state.a * this.state.b}
+            <br />
+            {"Div:" + this.state.a / this.state.b}
+            <br />
+          </div>
+        ) : (
+          <div>Click On Result button</div>
+        )}
       </div>
     );
   }
