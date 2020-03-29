dea50ae1f396ae8f00d5b0f2da7b174037271f94
siva <siva1978@gmail1.com>
2020-03-29 13:26:27 +0530

    Refactored Movies Component

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import Movie from "../movie/Movie";
 import "./styles.css";
+import movies from "./movies";
 class Movies extends React.Component {
-  state = {
-    Sarainodu: {
-      name: "Sarainodu",
-      likes: 0,
-      banner:
-        "https://www.filmibeat.com/fanimg/214x100x267/fan_images/sarrainodu-photos-images-41556.jpg"
-    },
-    Maharshi: {
-      name: "Maharshi",
-      likes: 0,
-      banner:
-        "https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg"
-    },
-    Avengers: {
-      name: "Avengers",
-      likes: 0,
-      banner:
-        "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
-    }
-  };
+  state = { ...movies };
   getMovie = movie => {
     return (
       <Movie name={movie.name} likes={movie.likes} banner={movie.banner} />
@@ -30,9 +12,9 @@ class Movies extends React.Component {
   render() {
     return (
       <div className="movies-section">
-        {this.getMovie(this.state.Sarainodu)}
-        {this.getMovie(this.state.Maharshi)}
-        {this.getMovie(this.state.Avengers)}
+        {this.getMovie(this.state.sarainodu)}
+        {this.getMovie(this.state.maharshi)}
+        {this.getMovie(this.state.avengers)}
       </div>
     );
   }
---
+++ src/Movies/movies.js
@@ -0,0 +1,21 @@
+const movies = {
+  sarainodu: {
+    name: "Sarainodu",
+    likes: 0,
+    banner:
+      "https://www.filmibeat.com/fanimg/214x100x267/fan_images/sarrainodu-photos-images-41556.jpg"
+  },
+  maharshi: {
+    name: "Maharshi",
+    likes: 0,
+    banner:
+      "https://m.media-amazon.com/images/M/MV5BNzQ3YzkyMjEtNGFmYi00Y2QzLTg4NjItODMzNWQ0NzBiYWEzXkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_.jpg"
+  },
+  avengers: {
+    name: "Avengers",
+    likes: 0,
+    banner:
+      "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg"
+  }
+};
+export default movies;
